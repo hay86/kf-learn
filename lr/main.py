9bcefeec3a09f0eecb935a801d101a5a9042b3ba
hay86 <xukaifeng1986@gmail.com>
2017-11-21 20:36:24 -0800

    add linear regression

--- lr/main.py
+++ lr/main.py
@@ -1,32 +1,33 @@
 import sys
 import numpy as np
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import train_test_split
-from src import LogisticRegression as KFLR
+from sklearn.datasets import make_classification, make_regression
+from src import LogisticRegression as MyLogisticRegression
+from src import LinearRegression as MyLinearRegression
 
 if len(sys.argv) < 2:
-	print 'Usage: python main.py test.csv'
+	print 'Usage: python main.py regression|classification'
 	sys.exit()
 
-df = pd.read_csv(sys.argv[1])
-col = df.columns
-
-X = df[col[1:-1]]
-X = np.array(X)
-Y = df['label']
-Y = np.array(Y)
+if sys.argv[1] == 'regression':
+	X, Y = make_regression(n_samples=100,n_features=5)
+	m1 = LinearRegression()
+	m2 = MyLinearRegression()
+	W = [0] * (len(X[0])+1)
+elif sys.argv[1] == 'classification':
+	X, Y = make_classification(n_samples=100,n_features=5)
+	m1 = LogisticRegression()
+	m2 = MyLogisticRegression()
+	W = [0] * len(X[0])
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.33)
 
 # lr by sklearn
-clf = LogisticRegression()
-clf.fit(X_train, Y_train)
-print '== test on sklearn:', clf.score(X_test,Y_test)
+m1.fit(X_train, Y_train)
+print '== test on sklearn:', m1.score(X_test,Y_test)
 
 # lr by kflearn
-clf = KFLR()
-W = [0] * len(X[0])
-W = clf.fit(X_train, Y_train, W, 0.1, 1000)
-print '== test on kflearn:', clf.score(X_test,Y_test, W)
+W = m2.fit(X_train, Y_train, W, 0.1, 1000)
+print '== test on kflearn:', m2.score(X_test,Y_test, W)
 
--- lr/src.py
+++ lr/src.py
@@ -16,18 +16,18 @@ class LogisticRegression:
 			err += Y[i] * math.log(hi) + (1-Y[i]) * math.log(1-hi)
 		return -err/m
 
-	def cost_function_derivative(self, X, Y, W, j, alpha):
+	def cost_function_derivative(self, X, Y, W, j):
 		err, m = 0, len(Y)
 		for i in xrange(m):
 			hi = self.hypothesis(W, X[i])
 			err += (hi - Y[i]) * X[i][j]
-		return err*alpha/m
+		return err/m
 
 	def gradient_descent(self, X, Y, W, alpha):
 		_W, m = [], len(Y)
 		for j in xrange(len(W)):
-			d = self.cost_function_derivative(X, Y, W, j, alpha)
-			_W.append(W[j] - d)
+			d = self.cost_function_derivative(X, Y, W, j)
+			_W.append(W[j] - d*alpha)
 		return _W
 
 	def fit(self, X, Y, W, alpha, n_iters, n_print = 10):
@@ -41,7 +41,7 @@ class LogisticRegression:
 		return W
 
 	def predict(self, X, W):
-		return round(self.hypothesis(X, W))
+		return round(self.hypothesis(W, X))
 
 	def score(self, X, Y, W):
 		score, m = 0, len(Y)
@@ -50,3 +50,53 @@ class LogisticRegression:
 			if label == Y[i]:
 				score += 1
 		return float(score)/m
+
+class LinearRegression:
+
+	def hypothesis(self, W, X):
+		m = len(X)
+		y = sum([W[i]*X[i] for i in xrange(m)])
+		return y + W[m]
+	
+	def cost_function(self, X, Y, W):
+		err, m = 0, len(Y)
+		for i in xrange(m):
+			hi = self.hypothesis(W, X[i])
+			err += (Y[i]-hi) ** 2
+		return err/m
+
+	def cost_function_derivative(self, X, Y, W, j):
+		err, m = 0, len(Y)
+		for i in xrange(m):
+			hi = self.hypothesis(W, X[i])
+			xj = X[i][j] if j < len(X[i]) else 1
+			err += (hi - Y[i]) * xj
+		return 2*err/m
+
+	def gradient_descent(self, X, Y, W, alpha):
+		_W, m = [], len(Y)
+		for j in xrange(len(W)):
+			d = self.cost_function_derivative(X, Y, W, j)
+			_W.append(W[j] - d*alpha)
+		return _W
+	
+	def fit(self, X, Y, W, alpha, n_iters, n_print = 10):
+		m, n = len(Y), n_iters/n_print
+		for i in xrange(n_iters):
+			W = self.gradient_descent(X, Y, W, alpha)
+			if i%n == 0 or i == n_iters-1:
+				print 'iter #', i
+				print 'weight is', W
+				print 'cost is', self.cost_function(X, Y, W)
+		return W
+
+	def predict(self, X, W):
+		return self.hypothesis(W, X)
+
+	def score(self, X, Y, W):
+		u, v, m, ym = 0, 0, len(Y), sum(Y)/len(Y)
+		for i in xrange(m):
+			hi = self.predict(X[i], W)
+			u += (Y[i]-hi) ** 2
+			v += (Y[i]-ym) ** 2
+		return 1 - u/v

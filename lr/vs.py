0e66d0e52460a81cf764ce0ad04bb0ed721bfac2
hay86 <xukaifeng1986@gmail.com>
2017-11-22 10:27:37 -0800

    minor

--- lr/vs.py
+++ lr/vs.py
@@ -1,5 +1,4 @@
 import sys
-import numpy as np
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification, make_regression
